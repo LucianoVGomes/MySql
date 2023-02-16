5f8ec0a8d844cd61c224b9954487de680c0f7ce4
unknown <patosanta1000@gmail.com>
2023-02-16 17:48:41 -0300

    trabalhando conceitos SQL

---
+++ atuallizarEstados.sql
@@ -0,0 +1,16 @@
+update estados1
+set nome = 'Maranhão'
+where sigla = 'MA'
+
+SELEct NOME FROM ESTADOS1 where sigla = 'MA'
+
+update estados1
+set nome = 'Paraná',
+populacao = 11.32
+WHERE SIGLA = 'PR'
+
+select nome,
+ sigla, 
+ populacao 
+ from estados1 
+ where sigla = 'PR'
