6c0f5bf55e0298dffb0386486c8f5d621433452e
unknown <patosanta1000@gmail.com>
2023-03-07 15:04:04 -0300

    Finalização do Banco de dados

---
+++ inserirPrefeitos.sql
@@ -0,0 +1,18 @@
+select * from cidades;
+select * from prefeitos;
+
+
+INSERT INTO prefeitos
+    (nome, cidade_id)
+VALUES 
+    ('Rodrigo Neves', 10),
+    ('Raquel Lyra', 11),
+    ('Zenaldo Coutinho', null);
+
+insert into prefeitos
+    (nome, cidade_id),
+VALUES
+    ('Rafael Greca', null);
+
+
+
